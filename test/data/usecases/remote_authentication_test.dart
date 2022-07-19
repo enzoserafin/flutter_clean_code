1658a1c84b509cf3391860633e6bd951327194b3
enzoserafin <enzoserafin@gmail.com>
2022-07-18 22:05:18 -0300

    ensure remote authentication throws unexpected error on 404

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -62,4 +62,21 @@ void main() {
     // Assert
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw unexpectedError if HttpClient returns 404', () async {
+    // Arrange
+    when(
+      httpClient.request(
+        url: anyNamed('url'),
+        method: anyNamed('method'),
+        body: anyNamed('body'),
+      ),
+    ).thenThrow(HttpError.noFound);
+
+    // Act
+    final future = sut.auth(params);
+
+    // Assert
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
