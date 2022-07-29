6c39de3057c4bffffef0504578542a2cd7d9309e
enzoserafin <enzoserafin@gmail.com>
2022-07-28 23:11:04 -0300

    ensure LoginPage presents error if password if invalid

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -12,18 +12,23 @@ class LoginPresenterSpy extends Mock implements LoginPresenter {}
 void main() {
   LoginPresenter presenter;
   StreamController<String> emailErrorController;
+  StreamController<String> passwordErrorController;
 
   Future<void> loadPage(WidgetTester tester) async {
     presenter = LoginPresenterSpy();
     emailErrorController = StreamController<String>();
+    passwordErrorController = StreamController<String>();
     when(presenter.emailErrorStream)
         .thenAnswer((_) => emailErrorController.stream);
+    when(presenter.passwordErrorStream)
+        .thenAnswer((_) => passwordErrorController.stream);
     final loginPage = MaterialApp(home: LoginPage(presenter));
     await tester.pumpWidget(loginPage);
   }
 
   tearDown(() {
     emailErrorController.close();
+    passwordErrorController.close();
   });
 
   testWidgets('Should load with correct initial state',
@@ -105,4 +110,42 @@ void main() {
       findsOneWidget,
     );
   });
+
+  testWidgets('Should present error if password is invalid',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    passwordErrorController.add('any error');
+    await tester.pump();
+
+    expect(find.text('any error'), findsOneWidget);
+  });
+
+  //   testWidgets('Should present no error if password is valid',
+  //     (WidgetTester tester) async {
+  //   await loadPage(tester);
+
+  //   emailErrorController.add(null);
+  //   await tester.pump();
+
+  //   expect(
+  //     find.descendant(
+  //         of: find.bySemanticsLabel('Email'), matching: find.byType(Text)),
+  //     findsOneWidget,
+  //   );
+  // });
+
+  // testWidgets('Should present no error if password is valid',
+  //     (WidgetTester tester) async {
+  //   await loadPage(tester);
+
+  //   emailErrorController.add('');
+  //   await tester.pump();
+
+  //   expect(
+  //     find.descendant(
+  //         of: find.bySemanticsLabel('Email'), matching: find.byType(Text)),
+  //     findsOneWidget,
+  //   );
+  // });
 }
