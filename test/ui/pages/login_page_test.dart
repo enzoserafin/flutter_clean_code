4329792fc0971d9ba022291861650768f9183b78
enzoserafin <enzoserafin@gmail.com>
2022-07-28 23:14:25 -0300

    ensure LoginPage presents no error if password is valid

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -121,31 +121,31 @@ void main() {
     expect(find.text('any error'), findsOneWidget);
   });
 
-  //   testWidgets('Should present no error if password is valid',
-  //     (WidgetTester tester) async {
-  //   await loadPage(tester);
+  testWidgets('Should present no error if password is valid',
+      (WidgetTester tester) async {
+    await loadPage(tester);
 
-  //   emailErrorController.add(null);
-  //   await tester.pump();
+    passwordErrorController.add(null);
+    await tester.pump();
 
-  //   expect(
-  //     find.descendant(
-  //         of: find.bySemanticsLabel('Email'), matching: find.byType(Text)),
-  //     findsOneWidget,
-  //   );
-  // });
+    expect(
+      find.descendant(
+          of: find.bySemanticsLabel('Senha'), matching: find.byType(Text)),
+      findsOneWidget,
+    );
+  });
 
-  // testWidgets('Should present no error if password is valid',
-  //     (WidgetTester tester) async {
-  //   await loadPage(tester);
+  testWidgets('Should present no error if password is valid',
+      (WidgetTester tester) async {
+    await loadPage(tester);
 
-  //   emailErrorController.add('');
-  //   await tester.pump();
+    passwordErrorController.add('');
+    await tester.pump();
 
-  //   expect(
-  //     find.descendant(
-  //         of: find.bySemanticsLabel('Email'), matching: find.byType(Text)),
-  //     findsOneWidget,
-  //   );
-  // });
+    expect(
+      find.descendant(
+          of: find.bySemanticsLabel('Senha'), matching: find.byType(Text)),
+      findsOneWidget,
+    );
+  });
 }
