0a8e462f6fca0605f2aac81acfdbf0f345963929
enzoserafin <enzoserafin@gmail.com>
2022-07-31 22:06:44 -0300

    ensure StreamLoginPresenter calls Validation on password changed

--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -5,6 +5,7 @@ import '../protocols/protocols.dart';
 
 class LoginState {
   String emailError;
+  // String passwordError;
 
   // Deixando o isFormValid apenas como getter (sem variável) nos economizamos memória
   // há apenas uma computação.
@@ -21,6 +22,9 @@ class StreamingLoginPresenter {
   Stream<String> get emailErrorStream =>
       _controller.stream.map((state) => state.emailError).distinct();
 
+  // Stream<String> get passwordErrorStream =>
+  //     _controller.stream.map((state) => state.passwordError).distinct();
+
   Stream<bool> get isFormValidStream =>
       _controller.stream.map((state) => state.isFormValid).distinct();
 
@@ -29,5 +33,9 @@ class StreamingLoginPresenter {
   void validateEmail(String email) {
     _state.emailError = validation.validate(field: 'email', value: email);
     _controller.add(_state);
+  }
+
+  void validatePassword(String password) {
+    validation.validate(field: 'password', value: password);
   }
 }
--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -11,6 +11,7 @@ void main() {
   StreamingLoginPresenter sut;
   ValidationSpy validation;
   String email;
+  String password;
 
   PostExpectation mockValidationCall(String field) => when(validation.validate(
       field: field ?? anyNamed('field'), value: anyNamed('value')));
@@ -23,6 +24,7 @@ void main() {
     validation = ValidationSpy();
     sut = StreamingLoginPresenter(validation: validation);
     email = faker.internet.email();
+    password = faker.internet.password();
     mockValidation();
   });
   test('Should call Validation with correct email', () {
@@ -54,5 +56,11 @@ void main() {
 
     sut.validateEmail(email);
     sut.validateEmail(email);
+  });
+
+  test('Should call Validation with correct password', () {
+    sut.validatePassword(password);
+
+    verify(validation.validate(field: 'password', value: password)).called(1);
   });
 }
