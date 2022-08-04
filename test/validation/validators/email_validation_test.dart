93d30ab5b70acffbe5a4500e2684965360b1ac6a
enzoserafin <enzoserafin@gmail.com>
2022-08-03 22:54:49 -0300

    ensure EmailValidation returns error if email is invalid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,6 +1,6 @@
+import 'package:test/test.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_clean_code/validation/protocols/field_validation.dart';
-import 'package:test/test.dart';
 
 class EmailValidation implements FieldValidation {
   final String field;
@@ -8,7 +8,10 @@ class EmailValidation implements FieldValidation {
   EmailValidation(this.field);
 
   String validate(String value) {
-    return null;
+    final regex = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
+    return isValid ? null : 'Campo inválido';
   }
 }
 
@@ -29,5 +32,9 @@ void main() {
 
   test('Should return null if email is valid', () {
     expect(sut.validate(faker.internet.email()), null);
+  });
+
+  test('Should return error if email is invalid', () {
+    expect(sut.validate(faker.person.name()), 'Campo inválido');
   });
 }
