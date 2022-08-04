6f215baa704ec8468d1b350e003a02290d94d6e9
enzoserafin <enzoserafin@gmail.com>
2022-08-03 22:36:17 -0300

    ensure EmailValidation returns null if email is empty or null

---
+++ test/validation/validators/email_validation_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_clean_code/validation/protocols/field_validation.dart';
+import 'package:test/test.dart';
+
+class EmailValidation implements FieldValidation {
+  final String field;
+
+  EmailValidation(this.field);
+
+  String validate(String value) {
+    return null;
+  }
+}
+
+void main() {
+  test('Should return null if email is empty', () {
+    final sut = EmailValidation('any_value');
+
+    expect(sut.validate(''), null);
+  });
+
+  test('Should return null if email is null', () {
+    final sut = EmailValidation('any_value');
+
+    expect(sut.validate(null), null);
+  });
+}
