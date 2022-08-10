9528d21638beca305ab47a3c91ad0f6db357b39f
enzoserafin <enzoserafin@gmail.com>
2022-08-09 22:39:37 -0300

    ensure LoginValidatinsFactory returns correct validations

---
+++ test/main/factories/pages/login/login_validation_factory.dart
@@ -0,0 +1,16 @@
+import 'package:test/test.dart';
+
+import 'package:flutter_clean_code/validation/validators/validators.dart';
+import 'package:flutter_clean_code/main/factories/factories.dart';
+
+void main() {
+  test('Should return the corret validations', () {
+    final validations = makeLoginValidations();
+
+    expect(validations, [
+      RequiredFieldValidation('email'),
+      EmailValidation('email'),
+      RequiredFieldValidation('password'),
+    ]);
+  });
+}
