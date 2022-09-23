ad3cc6efb2a65e6271e15b0f587f7523f5090136
enzoserafin <enzoserafin@gmail.com>
2022-09-23 00:20:13 -0300

    ensure LocalLoadCurrentAccount calls FetchSecureCacheStorage with correct value

---
+++ test/data/usecases/save_current_account/local_save_current_account_test.dart
@@ -0,0 +1,47 @@
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import 'package:flutter_clean_code/domain/helpers/helpers.dart';
+import 'package:flutter_clean_code/domain/entities/entities.dart';
+
+import 'package:flutter_clean_code/data/cache/cache.dart';
+import 'package:flutter_clean_code/data/usecases/usecases.dart';
+
+class SaveSecureCacheStorageSpy extends Mock implements SaveSecureCacheStorage {
+}
+
+void main() {
+  LocalSaveCurrentAccount sut;
+  SaveSecureCacheStorageSpy saveSecureCacheStorage;
+  AccountEntity account;
+
+  setUp(() {
+    saveSecureCacheStorage = SaveSecureCacheStorageSpy();
+    sut =
+        LocalSaveCurrentAccount(saveSecureCacheStorage: saveSecureCacheStorage);
+    account = AccountEntity(faker.guid.guid());
+  });
+
+  void mockError() {
+    when(saveSecureCacheStorage.saveSecure(
+            key: anyNamed('key'), value: anyNamed('value')))
+        .thenThrow(Exception());
+  }
+
+  test('Should call SaveSecureCacheStorage with corret values', () async {
+    sut.save(account);
+
+    verify(
+        saveSecureCacheStorage.saveSecure(key: 'token', value: account.token));
+  });
+
+  test('Should throw UnexpectedError if SaveSecureCacheStorage throws',
+      () async {
+    mockError();
+
+    final future = sut.save(account);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+}
