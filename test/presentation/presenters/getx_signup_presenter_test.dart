78d4af960de002a694d168f7b3ef54cbcb9b426f
enzoserafin <enzoserafin@gmail.com>
2022-12-20 22:02:33 -0300

    ensure getXSignUpPresenter emits error if addAccount fails

--- test/presentation/presenters/getx_signup_presenter_test.dart
+++ test/presentation/presenters/getx_signup_presenter_test.dart
@@ -41,6 +41,10 @@ void main() {
     mockAddAccountCall().thenAnswer((_) async => AccountEntity(token));
   }
 
+  void mockAddAccountError(DomainError error) {
+    mockAddAccountCall().thenThrow(error);
+  }
+
   PostExpectation mockSaveCurrentAccountCall() =>
       when(saveCurrentAccount.save(any));
 
@@ -284,13 +288,41 @@ void main() {
     await sut.signUp();
   });
 
-  test('Should emit correct events on Athentication success', () async {
+  test('Should emit correct events on AddAccount success', () async {
     sut.validateName(name);
     sut.validateEmail(email);
     sut.validatePassword(password);
     sut.validatePasswordConfirmation(passwordConfirmation);
 
     expectLater(sut.isLoadingStream, emits(true));
+
+    await sut.signUp();
+  });
+
+  test('Should emit correct events on EmailInUseCredentialsError', () async {
+    mockAddAccountError(DomainError.emailInUse);
+    sut.validateName(name);
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+    sut.validatePasswordConfirmation(passwordConfirmation);
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+    sut.mainErrorStream
+        .listen(expectAsync1((error) => expect(error, UIError.emailInUse)));
+
+    await sut.signUp();
+  });
+
+  test('Should emit correct events on Unexpected', () async {
+    mockAddAccountError(DomainError.unexpected);
+    sut.validateName(name);
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+    sut.validatePasswordConfirmation(passwordConfirmation);
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+    sut.mainErrorStream
+        .listen(expectAsync1((error) => expect(error, UIError.unexpected)));
 
     await sut.signUp();
   });
