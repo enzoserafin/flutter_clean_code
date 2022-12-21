135eade4ebdabf58589725a3879196786f1d34d9
enzoserafin <enzoserafin@gmail.com>
2022-12-20 21:08:00 -0300

    ensure getXSignUpPresenter emits isFormValid true if all fields are valid

--- lib/presentation/presenters/getx_signup_presenter.dart
+++ lib/presentation/presenters/getx_signup_presenter.dart
@@ -14,7 +14,11 @@ class GetxSignUpPresenter extends GetxController {
   var _passwordConfirmationError = Rx<UIError>();
   var _isFormValid = false.obs;
 
-  // Caso a stream receba o mesmo valor que o anterior ela não publica nova emissão de erro.
+  String _name;
+  String _email;
+  String _password;
+  String _passwordConfirmation;
+
   Stream<UIError> get emailErrorStream => _emailError.stream;
   Stream<UIError> get nameErrorStream => _nameError.stream;
   Stream<UIError> get passwordErrorStream => _passwordError.stream;
@@ -27,28 +31,39 @@ class GetxSignUpPresenter extends GetxController {
   });
 
   void validateEmail(String email) {
+    _email = email;
     _emailError.value = _validateField(field: 'email', value: email);
     _validateForm();
   }
 
   void validateName(String name) {
+    _name = name;
     _nameError.value = _validateField(field: 'name', value: name);
     _validateForm();
   }
 
   void validatePassword(String password) {
+    _password = password;
     _passwordError.value = _validateField(field: 'password', value: password);
     _validateForm();
   }
 
   void validatePasswordConfirmation(String passwordConfirmation) {
+    _passwordConfirmation = passwordConfirmation;
     _passwordConfirmationError.value = _validateField(
         field: 'passwordConfirmation', value: passwordConfirmation);
     _validateForm();
   }
 
   void _validateForm() {
-    _isFormValid.value = false;
+    _isFormValid.value = _emailError.value == null &&
+        _nameError.value == null &&
+        _passwordError.value == null &&
+        _passwordConfirmationError.value == null &&
+        _name != null &&
+        _email != null &&
+        _password != null &&
+        _passwordConfirmation != null;
   }
 
   UIError _validateField({String field, String value}) {
