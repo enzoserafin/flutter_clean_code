016d84a0dd82f8669e2d93b6126a47bfb0d01d36
enzoserafin <enzoserafin@gmail.com>
2022-12-22 09:51:42 -0300

    ensure RemoteLoadSurveys throws UnexpectedError if HttpClient return 404

--- test/data/usecases/load_surveys/remote_load_surveys_test.dart
+++ test/data/usecases/load_surveys/remote_load_surveys_test.dart
@@ -64,6 +64,10 @@ void main() {
     mockRequest().thenAnswer((_) async => data);
   }
 
+  void mockHttpError(HttpError error) {
+    mockRequest().thenThrow(error);
+  }
+
   setUp(() {
     url = faker.internet.httpUrl();
     httpClient = HttpClientSpy();
@@ -105,6 +109,14 @@ void main() {
     mockHttpData([
       {'invalid_key': 'invalid_value'}
     ]);
+
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw unexpectedError if HttpClient returns 404', () async {
+    mockHttpError(HttpError.noFound);
 
     final future = sut.load();
 
