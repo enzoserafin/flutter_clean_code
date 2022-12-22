bc4a633528a7bec90ddaf5f6811069d4bfd7c5bb
enzoserafin <enzoserafin@gmail.com>
2022-12-22 09:28:53 -0300

    ensure RemoteLoadSurveys returns 200 on success

--- lib/domain/entities/survey_entity.dart
+++ lib/domain/entities/survey_entity.dart
@@ -1,14 +1,19 @@
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
-class SurveyEntity {
+class SurveyEntity extends Equatable {
   final String id;
   final String question;
   final DateTime dateTime;
   final bool didAnswer;
+
   SurveyEntity({
     @required this.id,
     @required this.question,
     @required this.dateTime,
     @required this.didAnswer,
   });
+
+  @override
+  List<Object> get props => [id, question, dateTime, didAnswer];
 }
