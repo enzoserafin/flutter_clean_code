fec9e017de7da2e9a8448a2976cd7e08a82a3072
enzoserafin <enzoserafin@gmail.com>
2022-12-23 11:48:11 -0300

    ensure Decorator append header if header is not null

--- test/main/decorators/authorize_http_client_decorator_test.dart
+++ test/main/decorators/authorize_http_client_decorator_test.dart
@@ -20,7 +20,8 @@ class AuthorizeHttpClientDecorator {
     Map headers,
   }) async {
     final token = await fetchSecureCacheStorage.fetchSecure('token');
-    final authorizedHeaders = {'x-access-token': token};
+    final authorizedHeaders = headers ?? {}
+      ..addAll({'x-access-token': token});
     await decoratee.request(
         url: url, method: method, body: body, headers: authorizedHeaders);
   }
@@ -67,11 +68,26 @@ void main() {
 
   test('Should call decoratee with access token on header', () async {
     await sut.request(url: url, method: method, body: body);
-
     verify(httpClient.request(
+      url: url,
+      method: method,
+      body: body,
+      headers: {'x-access-token': token},
+    )).called(1);
+
+    await sut.request(
         url: url,
         method: method,
         body: body,
-        headers: {'x-access-token': token})).called(1);
+        headers: {'any_header': 'any_value'});
+    verify(httpClient.request(
+      url: url,
+      method: method,
+      body: body,
+      headers: {
+        'x-access-token': token,
+        'any_header': 'any_value',
+      },
+    )).called(1);
   });
 }
