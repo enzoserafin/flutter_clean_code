d77643c8e07c7cb54ba60bd1cc93558ce515c12b
enzoserafin <enzoserafin@gmail.com>
2022-12-26 17:43:51 -0300

    ensure LocalLoadSurveys calls CacheStorage.save with correct values

--- lib/data/usecases/load_surveys/local_load_surveys.dart
+++ lib/data/usecases/load_surveys/local_load_surveys.dart
@@ -17,7 +17,7 @@ class LocalLoadSurveys implements LoadSurveys {
         throw Exception();
       }
 
-      return _map(data);
+      return _mapToEntity(data);
     } catch (error) {
       throw DomainError.unexpected;
     }
@@ -26,13 +26,23 @@ class LocalLoadSurveys implements LoadSurveys {
   Future<void> validate() async {
     try {
       final data = await cacheStorage.fetch('surveys');
-      _map(data);
+      _mapToEntity(data);
     } on Exception {
       cacheStorage.delete('surveys');
     }
   }
 
-  List<SurveyEntity> _map(List<Map> list) => list
+  Future<void> save(List<SurveyEntity> surveys) async {
+    final value = _mapToJson(surveys);
+
+    await cacheStorage.save(key: 'surveys', value: value);
+  }
+
+  List<SurveyEntity> _mapToEntity(List<Map> list) => list
       .map<SurveyEntity>((json) => LocalSurveyModel.fromJson(json).toEntity())
+      .toList();
+
+  List<Map> _mapToJson(List<SurveyEntity> list) => list
+      .map((entity) => LocalSurveyModel.fromEntity(entity).toJson())
       .toList();
 }
