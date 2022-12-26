611a450648ce0d89a02386a36e6c49a8f8b4e1c7
enzoserafin <enzoserafin@gmail.com>
2022-12-26 14:49:07 -0300

    ensure LocalLoadSurveys throws UnexpectedError if cache is invalid

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -12,13 +12,18 @@ class LocalLoadSurveys {
   LocalLoadSurveys({@required this.fetchCacheStorage});
   Future<List<SurveyEntity>> load() async {
     final data = await fetchCacheStorage.fetch('surveys');
-    if (data?.isEmpty != false) {
+    try {
+      if (data?.isEmpty != false) {
+        throw Exception();
+      }
+
+      return data
+          .map<SurveyEntity>(
+              (json) => LocalSurveyModel.fromJson(json).toEntity())
+          .toList();
+    } catch (error) {
       throw DomainError.unexpected;
     }
-
-    return data
-        .map<SurveyEntity>((json) => LocalSurveyModel.fromJson(json).toEntity())
-        .toList();
   }
 }
 
@@ -92,6 +97,20 @@ void main() {
 
   test('Should throw UnexpectedErrorif cache is null', () async {
     mockFetch(null);
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpectedErrorif cache is invalid', () async {
+    mockFetch([
+      {
+        'id': faker.guid.guid(),
+        'question': faker.randomGenerator.string(10),
+        'date': 'invalid date',
+        'didAnswer': 'false',
+      },
+    ]);
     final future = sut.load();
 
     expect(future, throwsA(DomainError.unexpected));
