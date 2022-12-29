fcb216fdc75c36f1fb25bc563fac7143cddd6f6f
enzoserafin <enzoserafin@gmail.com>
2022-12-29 14:16:06 -0300

    move SurveyResult component to its own file

---
+++ lib/ui/pages/survey_result/components/survey_result.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class SurveyResult extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 4,
+      itemBuilder: (context, index) {
+        if (index == 0) {
+          return Container(
+              padding: EdgeInsets.only(
+                top: 40,
+                bottom: 20,
+                left: 20,
+                right: 20,
+              ),
+              decoration: BoxDecoration(
+                color: Theme.of(context).disabledColor.withAlpha(90),
+              ),
+              child: Text('Qual é seu framework web favorito?'));
+        }
+        return Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Theme.of(context).backgroundColor,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.network(
+                    'https://dwglogo.com/wp-content/uploads/2017/09/1460px-React_logo-1024x704.png',
+                    width: 40,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                      ),
+                      child: Text(
+                        'Angular',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ),
+                  Text('100%',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).primaryColorDark,
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 10,
+                    ),
+                    child: Icon(
+                      Icons.check_circle,
+                      color: Theme.of(context).highlightColor,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Divider(
+              height: 1,
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
