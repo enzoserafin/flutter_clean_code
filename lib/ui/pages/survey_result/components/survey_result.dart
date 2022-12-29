4d23cfba7ead425fb1713e9d8b5d7815e6551e53
enzoserafin <enzoserafin@gmail.com>
2022-12-29 16:16:45 -0300

    move components to their own files

--- lib/ui/pages/survey_result/components/survey_result.dart
+++ lib/ui/pages/survey_result/components/survey_result.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../survey_result.dart';
+import './components.dart';
 
 class SurveyResult extends StatelessWidget {
   final SurveyResultViewModel viewModel;
@@ -13,93 +14,10 @@ class SurveyResult extends StatelessWidget {
       itemCount: viewModel.answers.length + 1,
       itemBuilder: (context, index) {
         if (index == 0) {
-          return Container(
-              padding: EdgeInsets.only(
-                top: 40,
-                bottom: 20,
-                left: 20,
-                right: 20,
-              ),
-              decoration: BoxDecoration(
-                color: Theme.of(context).disabledColor.withAlpha(90),
-              ),
-              child: Text(viewModel.question));
+          return SurveyHeader(viewModel.question);
         }
-        return Column(
-          children: [
-            Container(
-              padding: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: Theme.of(context).backgroundColor,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  viewModel.answers[index - 1].image != null
-                      ? Image.network(
-                          viewModel.answers[index - 1].image,
-                          width: 40,
-                        )
-                      : SizedBox.shrink(),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 10,
-                      ),
-                      child: Text(
-                        viewModel.answers[index - 1].answer,
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ),
-                  ),
-                  Text(viewModel.answers[index - 1].percent,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).primaryColorDark,
-                      )),
-                  viewModel.answers[index - 1].isCurrentAnswer
-                      ? ActiveIcon()
-                      : DisableIcon(),
-                ],
-              ),
-            ),
-            Divider(
-              height: 1,
-            ),
-          ],
-        );
+        return SurveyAnswer(viewModel.answers[index - 1]);
       },
-    );
-  }
-}
-
-class ActiveIcon extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(
-        left: 10,
-      ),
-      child: Icon(
-        Icons.check_circle,
-        color: Theme.of(context).highlightColor,
-      ),
-    );
-  }
-}
-
-class DisableIcon extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(
-        left: 10,
-      ),
-      child: Icon(
-        Icons.check_circle,
-        color: Theme.of(context).disabledColor,
-      ),
     );
   }
 }
