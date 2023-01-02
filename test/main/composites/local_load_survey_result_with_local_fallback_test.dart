386043b6e75ed5095c673b1afce04107dbf87271
enzoserafin <enzoserafin@gmail.com>
2023-01-02 15:01:25 -0300

    move sut creation to the setup method

--- test/main/composites/local_load_survey_result_with_local_fallback_test.dart
+++ test/main/composites/local_load_survey_result_with_local_fallback_test.dart
@@ -19,11 +19,17 @@ class RemoteLoadSurveyResultSpy extends Mock implements RemoteLoadSurveyResult {
 }
 
 void main() {
-  test('Should call remote LoadBySurvey', () async {
-    final surveyId = faker.guid.guid();
-    final remote = RemoteLoadSurveyResultSpy();
-    final sut = RemoteLoadSurveyResultWithLocalFallback(remote: remote);
+  RemoteLoadSurveyResultWithLocalFallback sut;
+  RemoteLoadSurveyResultSpy remote;
+  String surveyId;
 
+  setUp(() {
+    surveyId = faker.guid.guid();
+    remote = RemoteLoadSurveyResultSpy();
+    sut = RemoteLoadSurveyResultWithLocalFallback(remote: remote);
+  });
+
+  test('Should call remote LoadBySurvey', () async {
     await sut.loadBySurvey(surveyId: surveyId);
 
     verify(remote.loadBySurvey(surveyId: surveyId)).called(1);
