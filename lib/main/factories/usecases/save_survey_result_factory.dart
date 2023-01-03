43030bab14448ba4389bba2f39282791b771a228
enzoserafin <enzoserafin@gmail.com>
2023-01-03 13:38:39 -0300

    add SaveSurveyResult composition

---
+++ lib/main/factories/usecases/save_survey_result_factory.dart
@@ -0,0 +1,9 @@
+import '../../../data/usecases/usecases.dart';
+import '../../../domain/usecases/usecases.dart';
+import '../factories.dart';
+
+SaveSurveyResult makeRemoteSaveSurveyResult(String surveyId) =>
+    RemoteSaveSurveyResult(
+      httpClient: makeAuthorizeHttpClientDecorator(),
+      url: makeApiUrl('surveys_result/$surveyId/results'),
+    );
