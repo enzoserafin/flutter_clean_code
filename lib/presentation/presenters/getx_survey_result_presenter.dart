eab4c41212417f34e7dc0fbc8c5ae9a897d5ce4c
enzoserafin <enzoserafin@gmail.com>
2023-01-03 12:07:29 -0300

    remove duplicated code

--- lib/presentation/presenters/getx_survey_result_presenter.dart
+++ lib/presentation/presenters/getx_survey_result_presenter.dart
@@ -27,40 +27,18 @@ class GetxSurveyResultPresenter extends GetxController
       @required this.surveyId});
 
   Future<void> loadData() async {
-    try {
-      isLoading = true;
-
-      SurveyResultEntity surveyResult =
-          await loadSurveyResult.loadBySurvey(surveyId: surveyId);
-      _surveyResult.value = SurveyResultViewModel(
-        surveyId: surveyResult.surveyId,
-        question: surveyResult.question,
-        answers: surveyResult.answers
-            .map((answer) => SurveyAnswerViewModel(
-                  image: answer.image,
-                  answer: answer.answer,
-                  isCurrentAnswer: answer.isCurrentAnswer,
-                  percent: '${answer.percent}%',
-                ))
-            .toList(),
-      );
-    } on DomainError catch (error) {
-      if (error == DomainError.accessDenied) {
-        isSessionExpired = true;
-      } else {
-        _surveyResult.subject.addError(UIError.unexpected.description);
-      }
-    } finally {
-      isLoading = false;
-    }
+    showResultOnAction(() => loadSurveyResult.loadBySurvey(surveyId: surveyId));
   }
 
   Future<void> save({String answer}) async {
+    showResultOnAction(() => saveSurveyResult.save(answer: answer));
+  }
+
+  Future<void> showResultOnAction(Future<SurveyResultEntity> action()) async {
     try {
       isLoading = true;
 
-      SurveyResultEntity surveyResult =
-          await saveSurveyResult.save(answer: answer);
+      SurveyResultEntity surveyResult = await action();
 
       _surveyResult.value = SurveyResultViewModel(
         surveyId: surveyResult.surveyId,
