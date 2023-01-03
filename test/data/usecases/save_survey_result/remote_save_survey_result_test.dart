8a0785535b11d46e8847262c48f78ef78ea2f255
enzoserafin <enzoserafin@gmail.com>
2023-01-02 22:48:23 -0300

    ensure RemoteSaveSurveyResult returns surveyResult on success

--- test/data/usecases/save_survey_result/remote_save_survey_result_test.dart
+++ test/data/usecases/save_survey_result/remote_save_survey_result_test.dart
@@ -3,7 +3,7 @@ import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
 import 'package:flutter_clean_code/domain/helpers/helpers.dart';
-// import 'package:flutter_clean_code/domain/entities/entities.dart';
+import 'package:flutter_clean_code/domain/entities/entities.dart';
 
 import 'package:flutter_clean_code/data/http/http.dart';
 import 'package:flutter_clean_code/data/usecases/usecases.dart';
@@ -15,12 +15,39 @@ void main() {
   HttpClientSpy httpClient;
   String url;
   String answer;
+  Map surveyResult;
+
+  Map mockValidData() => {
+        'surveyId': faker.guid.guid(),
+        'question': faker.randomGenerator.string(50),
+        'answers': [
+          {
+            'image': faker.internet.httpUrl(),
+            'answer': faker.randomGenerator.string(20),
+            'percent': faker.randomGenerator.integer(100),
+            'count': faker.randomGenerator.integer(1000),
+            'isCurrentAccountAnswer': faker.randomGenerator.boolean()
+          },
+          {
+            'answer': faker.randomGenerator.string(20),
+            'percent': faker.randomGenerator.integer(100),
+            'count': faker.randomGenerator.integer(1000),
+            'isCurrentAccountAnswer': faker.randomGenerator.boolean()
+          },
+        ],
+        'date': faker.date.dateTime().toIso8601String(),
+      };
 
   PostExpectation mockRequest() => when(httpClient.request(
         url: anyNamed('url'),
         method: anyNamed('method'),
         body: anyNamed('body'),
       ));
+
+  void mockHttpData(Map data) {
+    surveyResult = data;
+    mockRequest().thenAnswer((_) async => data);
+  }
 
   void mockHttpError(HttpError error) {
     mockRequest().thenThrow(error);
@@ -31,6 +58,7 @@ void main() {
     url = faker.internet.httpUrl();
     httpClient = HttpClientSpy();
     sut = RemoteSaveSurveyResult(url: url, httpClient: httpClient);
+    mockHttpData(mockValidData());
   });
 
   test('Should call HttpClient with correct values', () async {
@@ -38,6 +66,32 @@ void main() {
 
     verify(
         httpClient.request(url: url, method: 'put', body: {'answer': answer}));
+  });
+
+  test('Should return surveyResult on 200', () async {
+    final result = await sut.save();
+
+    expect(
+        result,
+        SurveyResultEntity(
+          surveyId: surveyResult['surveyId'],
+          question: surveyResult['question'],
+          answers: [
+            SurveyAnswerEntity(
+              image: surveyResult['answers'][0]['image'],
+              answer: surveyResult['answers'][0]['answer'],
+              isCurrentAnswer: surveyResult['answers'][0]
+                  ['isCurrentAccountAnswer'],
+              percent: surveyResult['answers'][0]['percent'],
+            ),
+            SurveyAnswerEntity(
+              answer: surveyResult['answers'][1]['answer'],
+              isCurrentAnswer: surveyResult['answers'][1]
+                  ['isCurrentAccountAnswer'],
+              percent: surveyResult['answers'][1]['percent'],
+            ),
+          ],
+        ));
   });
 
   test('Should throw unexpectedError if HttpClient returns 404', () async {
